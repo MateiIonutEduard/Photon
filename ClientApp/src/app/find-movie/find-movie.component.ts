2ad7d3b0df1fab1d498411faa1fb24a11e14e1dd
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-02-06 22:20:36 +0200

    Fix too much Ajax calls #6

--- ClientApp/src/app/find-movie/find-movie.component.ts
+++ ClientApp/src/app/find-movie/find-movie.component.ts
@@ -3,7 +3,7 @@ import {Movie, MovieService} from "../services/movie/movie.service";
 import {Router} from "@angular/router";
 import {GenreService} from "../services/genre/genre.service";
 import {NavigateService} from "../services/navigate/navigate.service";
-import {NotifyService} from "../services/nofity/notify.service";
+import { NotifyService } from "../services/nofity/notify.service";
 
 @Component({
   selector: 'app-find-movie',
@@ -34,22 +34,11 @@ export class FindMovieComponent implements OnDestroy {
       50);
   }
 
-  UpdateMovies(): any {
+  UpdateMovies(): any { 
     this.notify.GetSignal().subscribe(res => {
-      if (res == -1) {
-        let model = this.genreService.GetModel();
-        this.movieService.SearchMovies(model).subscribe(res => {
-          this.movies = res.movies;
-          this.navigate.SetPage(res.movies.length,1, res.pages);
-          
-          for (let k = 0; k < this.movies.length; k++) {
-            let url = this.movies[k].info.image_url;
-            if (url == undefined) this.movies[k].info.image_url = '/assets/cinema.png';
-          }
-        });
-      } else if (res == 1) {
+      if (res == 1) {
         this.navigate.GetPage().subscribe(res => {
-          if (this.page != res.page) {
+          if (this.page !== res.page) {
             this.page = res.page;
             const model = this.genreService.GetModel();
             this.movieService.SearchPageMovies(this.page, model).subscribe(res => {
